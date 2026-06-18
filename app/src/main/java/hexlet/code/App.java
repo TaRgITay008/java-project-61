2da082808383d66330f61a8f399ec767aa23031f
Targitay <cibrenok@bk.ru>
2026-06-18 09:24:55 +0300

    fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,10 @@
 package hexlet.code;
 
-import hexlet.code.games.EvenGame;
 import hexlet.code.games.CalcGame;
+import hexlet.code.games.EvenGame;
 import hexlet.code.games.GcdGame;
-import hexlet.code.games.ProgressionGame;
 import hexlet.code.games.PrimeGame;
+import hexlet.code.games.ProgressionGame;
 import java.util.Scanner;
 
 public class App {
@@ -46,7 +46,7 @@ public class App {
                 System.out.println("Goodbye!");
                 break;
             default:
-                System.out.println("Invalid choice. Please try again.");
+                System.out.println("Invalid choice '" + choice + "'. Please enter a number between 0 and 6.");
                 break;
         }
 
